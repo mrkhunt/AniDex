40d1bcb8979a90960dcf4520d4d01bf790db314b
Lucas Toh <tohhylucas@gmail.com>
2024-03-30 16:45:31 -0400

    Implement camera feature and add alerts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,9 +36,9 @@
 // export default UploadImage;
 
 import React, { useState } from "react";
-import axios from "axios";
 import CameraPage from "./pages/camera";
 import Pokedex from "./pages/pokedex";
+import { Button } from "rsuite";
 
 function App() {
   const [page, setPage] = useState("camera");
@@ -50,12 +50,35 @@ function App() {
   return (
     <div>
       <header>
-        <nav>
-          <button onClick={() => handlePageChange("camera")}>Camera</button>
-          <button onClick={() => handlePageChange("pokedex")}>Pokedex</button>
-        </nav>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "10px",
+            marginTop: "10px",
+            marginBottom: "10px",
+          }}
+        >
+          <h1>AnimaDex</h1>
+        </div>
       </header>
       {page === "camera" ? <CameraPage /> : <Pokedex />}
+      <footer>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "10px",
+            marginTop: "10px",
+            marginBottom: "10px",
+          }}
+        >
+          <nav>
+            <Button onClick={() => handlePageChange("camera")}>Camera</Button>
+            <Button onClick={() => handlePageChange("pokedex")}>Pokedex</Button>
+          </nav>
+        </div>
+      </footer>
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(express.static("frontend/dist"));
+
 import { initializeApp } from "firebase/app";
 import {
   getFirestore,
